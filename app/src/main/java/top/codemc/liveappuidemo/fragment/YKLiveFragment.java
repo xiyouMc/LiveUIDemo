29cd143a80cd9c5c0c8062b343b3802fc7335917
Mc <694449212@qq.com>
2016-08-16 14:35:33 +0800

    add user container

--- app/src/main/java/top/codemc/liveappuidemo/fragment/YKLiveFragment.java
+++ app/src/main/java/top/codemc/liveappuidemo/fragment/YKLiveFragment.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
+import android.content.res.TypedArray;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,12 +39,15 @@ public class YKLiveFragment extends Fragment implements LayoutAdapter.OnRecycler
     private RelativeLayout root_view;
     private ListView liveChatListView;
     private LiveChatListAdapter liveChatListAdapter;
-    private RelativeLayout bollow_msg_rl;
-    private LinearLayout live_bollow_ln;
+    private RelativeLayout below_msg_rl;
+    private LinearLayout live_below_ln;
     private EditText msg_edit_text;
     private Button msg_send_bt;
     private int screenHeight = 0;
     private int keyHeight = 0;
+    private RecyclerView usersView;
+    private LayoutAdapter userContainerAdapter;
+    private ImageView img_room_creator;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -53,18 +55,16 @@ public class YKLiveFragment extends Fragment implements LayoutAdapter.OnRecycler
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onViewCreated(View rootView, Bundle savedInstanceState) {
+        super.onViewCreated(rootView, savedInstanceState);
         final Activity activity = getActivity();
 
-        root_view = (RelativeLayout) view.findViewById(R.id.root_view);
+        initView(rootView);
+        initViewData();
+
         screenHeight = activity.getWindowManager().getDefaultDisplay().getHeight();
         keyHeight = screenHeight / 3;
-        //top view
-        RecyclerView usersView = (RecyclerView) view.findViewById(R.id.listview_users);
-        usersView.setHasFixedSize(true);
-        usersView.setLongClickable(false);
-        configRecyclerView(activity, usersView);
+
 
 //        DisplayMetrics metrics = new DisplayMetrics();
 //        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
@@ -89,34 +89,42 @@ public class YKLiveFragment extends Fragment implements LayoutAdapter.OnRecycler
 //            }
 //        });
 
+    }
+
+    private void initView(View rootView) {
+        root_view = (RelativeLayout) rootView.findViewById(R.id.root_view);
+
+        img_room_creator = (ImageView) rootView.findViewById(R.id.img_room_creator);
+        //top view
+        usersView = (RecyclerView) rootView.findViewById(R.id.listview_users);
         //msg view
-        liveChatListView = (ListView) view.findViewById(R.id.live_chatlist);
-        liveChatListAdapter = new LiveChatListAdapter();
-        liveChatListView.setAdapter(liveChatListAdapter);
-
-
+        liveChatListView = (ListView) rootView.findViewById(R.id.live_chatlist);
         //bollow view
-        live_bollow_ln = (LinearLayout) view.findViewById(R.id.live_bollow_ln);
-        ImageView live_send_msg = (ImageView) view.findViewById(R.id.live_send_msg);
-        ImageView live_send_gift = (ImageView) view.findViewById(R.id.live_send_gift);
-        ImageView live_share = (ImageView) view.findViewById(R.id.live_share);
-        bollow_msg_rl = (RelativeLayout) view.findViewById(R.id.send_rl);
-        msg_edit_text = (EditText) view.findViewById(R.id.live_msg);
-//        msg_edit_text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if (hasFocus) {
-//                    activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-//                    //pop.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-//                }
-//            }
-//        });
-        msg_send_bt = (Button) view.findViewById(R.id.live_send);
-        msg_send_bt.setOnTouchListener(this);
+        live_below_ln = (LinearLayout) rootView.findViewById(R.id.live_bollow_ln);
+        ImageView live_send_msg = (ImageView) rootView.findViewById(R.id.live_send_msg);
+        ImageView live_send_gift = (ImageView) rootView.findViewById(R.id.live_send_gift);
+        ImageView live_share = (ImageView) rootView.findViewById(R.id.live_share);
+        below_msg_rl = (RelativeLayout) rootView.findViewById(R.id.send_rl);
+        msg_edit_text = (EditText) rootView.findViewById(R.id.live_msg);
+        msg_send_bt = (Button) rootView.findViewById(R.id.live_send);
         live_send_msg.setOnTouchListener(this);
         live_send_gift.setOnTouchListener(this);
         live_share.setOnTouchListener(this);
+        msg_send_bt.setOnTouchListener(this);
+    }
 
+    private void initViewData() {
+        //userContainer
+        usersView.setHasFixedSize(true);
+        usersView.setLongClickable(false);
+        configRecyclerView(getActivity(), usersView);
+        //msg adapter
+        liveChatListAdapter = new LiveChatListAdapter();
+        liveChatListView.setAdapter(liveChatListAdapter);
+
+        //test userContainer
+        configUserContainer();
+        img_room_creator.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.live_user));
     }
 
     private void configRecyclerView(Activity activity, RecyclerView usersView) {
@@ -124,17 +132,22 @@ public class YKLiveFragment extends Fragment implements LayoutAdapter.OnRecycler
         LinearLayoutManager layoutManager = new LinearLayoutManager(activity);
         layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         usersView.setLayoutManager(layoutManager);
-        Bitmap bmp = BitmapFactory.decodeResource(resources, R.drawable.user_img);
-        UsersContainerModel model = new UsersContainerModel.Builder().userProtrait(bmp).userType(bmp).build();
-        LayoutAdapter adapter = new LayoutAdapter(activity);
-        adapter.setOnRecyclerViewListener(this);
-        usersView.setAdapter(adapter);
-        for (int index = 0; index < 100; index++) {
-            adapter.addItem(index, model);
-        }
-        adapter.notifyDataSetChanged();
+        userContainerAdapter = new LayoutAdapter(activity);
+        userContainerAdapter.setOnRecyclerViewListener(this);
+        usersView.setAdapter(userContainerAdapter);
+        userContainerAdapter.notifyDataSetChanged();
         int spacingInPixels = resources.getDimensionPixelOffset(R.dimen.space);
         usersView.addItemDecoration(new RvSpaceItemDecoration(spacingInPixels));
+    }
+
+    private void configUserContainer() {
+        TypedArray userResArray = getResources().obtainTypedArray(R.array.test_users);
+        for (int index = 0; index < userResArray.length(); index++) {
+            userContainerAdapter.addItem(index, new UsersContainerModel.Builder()
+                    .userProtrait(BitmapFactory.decodeResource(getResources(), userResArray.getResourceId(index, -1))).build());
+        }
+        userContainerAdapter.notifyDataSetChanged();
+        userResArray.recycle();
     }
 
     @Override
@@ -154,8 +167,8 @@ public class YKLiveFragment extends Fragment implements LayoutAdapter.OnRecycler
 
         switch (id) {
             case R.id.live_send_msg:
-                live_bollow_ln.setVisibility(View.GONE);
-                bollow_msg_rl.setVisibility(View.VISIBLE);
+                live_below_ln.setVisibility(View.GONE);
+                below_msg_rl.setVisibility(View.VISIBLE);
                 msg_edit_text.requestFocus();
                 final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED);
@@ -166,9 +179,6 @@ public class YKLiveFragment extends Fragment implements LayoutAdapter.OnRecycler
                 return true;
             case R.id.live_send:
                 msg_edit_text.requestFocus();
-//                imm.hideSoftInputFromWindow(msg_edit_text.getWindowToken(), 0);
-//                bollow_msg_rl.setVisibility(View.GONE);
-//                live_bollow_ln.setVisibility(View.VISIBLE);
                 return true;
         }
         return false;
@@ -178,8 +188,8 @@ public class YKLiveFragment extends Fragment implements LayoutAdapter.OnRecycler
     public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
         if (oldBottom != 0 && bottom != 0 && (oldBottom - bottom > keyHeight)) {
         } else if (oldBottom != 0 && bottom != 0 && (bottom - oldBottom > keyHeight)) {
-            bollow_msg_rl.setVisibility(View.GONE);
-            live_bollow_ln.setVisibility(View.VISIBLE);
+            below_msg_rl.setVisibility(View.GONE);
+            live_below_ln.setVisibility(View.VISIBLE);
         }
     }
 
